59bf8507e78432f8d5a51c730ba5a6175f03ef25
BoyanP <BoyanP@users.noreply.github.com>
2023-09-12 00:28:09 -0600

    created a react project using vite and added a bunch of components for the site

---
+++ src/components/TopBannerImage.jsx
@@ -0,0 +1,9 @@
+const BannerImage = ()=> {
+
+    return (
+        <div className="BannerImage" id="Home">
+            <h1> Bobby Tarian</h1>
+        </div>
+    );
+}
+export default BannerImage;
