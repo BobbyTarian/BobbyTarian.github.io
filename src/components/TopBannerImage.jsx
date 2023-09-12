a6a92da7b9d2c1660cb970e63991972952fa0a0b
BoyanP <BoyanP@users.noreply.github.com>
2023-09-12 17:17:59 -0600

    added actual links for the brand buttons and updated the banner image and added some new text

--- src/components/TopBannerImage.jsx
+++ src/components/TopBannerImage.jsx
@@ -1,7 +1,10 @@
+import bannerImage from "../assets/bannerImage.jpg";
+
+
 const BannerImage = ()=> {
 
     return (
-        <div className="BannerImage" id="Home">
+        <div className="BannerImage" style={{backgroundImage: "url(" + bannerImage + ")"}} id="Home">
             <h1> Bobby Tarian</h1>
         </div>
     );
