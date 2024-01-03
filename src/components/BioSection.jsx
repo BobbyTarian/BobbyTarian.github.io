29eb86d1397e7c01a4f6bdef9a776e4c10ffc5d1
BoyanP <BoyanP@users.noreply.github.com>
2024-01-02 19:18:25 -0700

    updating my bio

--- src/components/BioSection.jsx
+++ src/components/BioSection.jsx
@@ -3,10 +3,17 @@ const bioSection = ()=> {
         return (
             <section className="BioSection" id="Bio">
                 <div className="BioText">
-                Nostalgia, dreamscapes, umbrage, sweet memories. 
-                Bobby Tarian ignites imagination through playful melodies and his lyricism.
-                The homegrown Alberta boy melds synth-pop into a singer-songwriter style.
-                Moon Remains, Seasons Change is Bobby Tarian's debut EP and came out November, 2020.
+                    Bobby Tarian captivates audiences with his tender lyrics and humility. 
+                    Born and raised in Edmonton, his dreamy sound and storytelling is informed by introspection and the resplendent prairie sunsets.
+                    His genre-blending indie-folk-synth-rock style is influenced by Sufjan Stevens, Tame Impala, The Strokes, and Andy Shauf.
+                    Moon Remains Seasons Change, Bobby’s debut EP, received significant radio play on college radio stations across North America.
+                    The track titled ”Remember” from this EP is featured in Michelle Vance Hehir’s theatre production “One Polaroid”. 
+                    You can catch Bobby performing at various venues, open mics, and farmers markets in Edmonton. 
+                    <br></br>
+                    <hr></hr>
+                    Nostalgia, dreamscapes, sweet memories - Bobby Tarian ignites imagination through his playful melodies and lyricism.
+                    The homegrown Alberta boy melds synth-pop into a singer-songwriter style. 
+                    Moon Remains, Seasons Change is Bobby Tarian’s debut EP, released November 2020. 
                 </div>
             </section>
         );
