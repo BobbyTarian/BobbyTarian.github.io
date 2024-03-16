64ea10bf063f588ec1ce889dc1872db152f1b017
BoyanP <BoyanP@users.noreply.github.com>
2024-03-15 21:01:19 -0600

    fixing embedded video width. fixed 404 errors with navbar links. added home page button on my links

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,52 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import LinkView from './pages/LinkView.jsx'
+import musicSection from './components/MusicSection.jsx'
+import BannerImage from './components/TopBannerSection.jsx'
+import ShowsSection from './components/ShowsSection.jsx'
+import bioSection from './components/BioSection.jsx'
+import contactSection from './components/ContactSection.jsx'
 import './index.css'
 import {
   createHashRouter,
   RouterProvider,
 } from "react-router-dom";
 
+
+
 const router = createHashRouter([
   {
     path: "/",
     element: <App></App>,
+    errorElement: <LinkView></LinkView>,
+    children: [{
+      path:"Music",
+      element: <musicSection></musicSection>
+      },
+      {
+        path:"Home",
+        element:<BannerImage></BannerImage>
+      },
+      {
+        path:"Shows",
+        element:<ShowsSection></ShowsSection>
+      },
+      {
+        path:"Bio",
+        element:<bioSection></bioSection>
+      },
+      {
+        path:"Contact",
+        element:<contactSection></contactSection>
+      }
+    ]
   },
   {
     path: "links",
-    element: <LinkView></LinkView>
-  }
+    element: <LinkView></LinkView>,
+    errorElement: <LinkView></LinkView>
+  },
+
 ]);
 
 
