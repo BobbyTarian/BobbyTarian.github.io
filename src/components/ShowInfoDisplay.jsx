5d120672e74d4766d3a938d3d84273e474d1ca6f
BoyanP <BoyanP@users.noreply.github.com>
2024-05-21 23:44:42 -0600

    adding more shows, updated the styling when there is not link for tickets

--- src/components/ShowInfoDisplay.jsx
+++ src/components/ShowInfoDisplay.jsx
@@ -1,16 +1,32 @@
 import { propTypes } from "react-bootstrap/esm/Image";
 const ShowInfoDisplay = (props) => {
+  if( props.ticketLink!= null ) {
 
-    return (
-    <div className="showInfoDisplay">
-        <div className="ShowInfo">
-                <a className="ShowLink"  href={props.ticketLink} target="_blank" rel="noreferrer" >
-                     {props.location}
-                </a> 
-                <p className= "ShowDate"> {props.date}</p>
-            </div>
+  
 
-    </div>);
+      return (
+      <div className="showInfoDisplay">
+          <div className="ShowInfo">
+                  <a className="ShowLink"  href={props.ticketLink} target="_blank" rel="noreferrer" >
+                      {props.location}
+                  </a> 
+                  <p className= "ShowDate"> {props.date}</p>
+              </div>
+
+      </div>);
+    }
+    else {
+         return (
+      <div className="showInfoDisplay">
+          <div className="ShowInfo">
+                  <a className="ShowLink noLink" >
+                      {props.location}
+                  </a> 
+                  <p className= "ShowDate"> {props.date}</p>
+              </div>
+
+      </div>);
+    }
 } 
 
 ShowInfoDisplay.propTypes = {
--- src/components/TopBannerSection.jsx
+++ src/components/TopBannerSection.jsx
@@ -1,18 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import bannerImage from "../assets/bannerImage.jpg";
 import headshotImage from "../assets/XPRO8190-cropped.jpg";
 import StreamingPlatformsDisplay from "./StreamingPlatformsDisplay";
 
 const BannerImage = ()=> {
     const [width, setWidth] = useState(0);
-    const onLoad = () => {
-    if ( window.innerWidth < 560 ) {
-        setWidth(window.innerWidth - 10);
+
+    useEffect(() => {
+    const handleResize = () => {
+      // Perform actions on scroll
+      if ( window.innerWidth < 560 ) {
+        setWidth(window.innerWidth - 50);
     }
     else {
         setWidth(560);
     }
-  };
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
     return (
         <div className="BannerImage" style={{backgroundImage: "url(" + bannerImage + ")", opacity:0.65}} id="Home">
             <h1 className="pageTitle"> Bobby Tarian</h1>
@@ -20,7 +29,7 @@ const BannerImage = ()=> {
             <section className="videoSection">
                 <h2> Here For you lyric video</h2>
                 <div className="youtubeVid">
-                    <iframe onLoad={onLoad} width={width} height="315" src="https://www.youtube.com/embed/pX-PYqUUbJk?si=uokqmZ7aZv2c7kyA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    <iframe width={width} height="315" src="https://www.youtube.com/embed/pX-PYqUUbJk?si=uokqmZ7aZv2c7kyA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                 </div>
                 
             </section>
