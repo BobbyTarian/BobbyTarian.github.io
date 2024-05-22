b5fa4e02cfa5f8c00c01cc0ead352fa338e408f3
BoyanP <BoyanP@users.noreply.github.com>
2024-05-22 01:11:54 -0600

    was trying to remove some errors showing up. also chrome still is acting weird

--- src/components/TopBannerSection.jsx
+++ src/components/TopBannerSection.jsx
@@ -9,8 +9,8 @@ const BannerImage = ()=> {
     useEffect(() => {
     const handleResize = () => {
       // Perform actions on scroll
-      if ( window.innerWidth < 560 ) {
-        setWidth(window.innerWidth - 50);
+      if ( window.visualViewport.width < 560 ) {
+        setWidth(window.visualViewport.width - 50);
     }
     else {
         setWidth(560);
@@ -18,21 +18,21 @@ const BannerImage = ()=> {
     };
     ['resize', 'load'].forEach( function(event){
         window.addEventListener(event, handleResize);
-        return () => {
-      window.removeEventListener(event, handleResize);
-    };
     });
     
   }, []);
-
+    const videoSectionStyle = {
+        width: width,
+        height: 315
+    };
     return (
         <div className="BannerImage" style={{backgroundImage: "url(" + bannerImage + ")", opacity:0.65}} id="Home">
             <h1 className="pageTitle"> Bobby Tarian</h1>
             
-            <section className="videoSection">
+            <section className="videoSection" style={videoSectionStyle}>
                 <h2> Here For you lyric video</h2>
                 <div className="youtubeVid">
-                    <iframe width={width} height="315" src="https://www.youtube.com/embed/pX-PYqUUbJk?si=uokqmZ7aZv2c7kyA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    <iframe width={width} height="315" src="https://www.youtube.com/embed/pX-PYqUUbJk?si=uokqmZ7aZv2c7kyA" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
                 </div>
                 
             </section>
