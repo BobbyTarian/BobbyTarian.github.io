6fb012af3269716bc76d00f9ff8f645f5d7580d0
BoyanP <BoyanP@users.noreply.github.com>
2025-03-25 17:20:30 -0600

    trying out the pixel here instead

--- src/utils/metaPixel.jsx
+++ src/utils/metaPixel.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import config from '../../config';
+import config from "../config";
 const MetaPixel = () => {
   useEffect(() => {
     !(function (f, b, e, v, n, t, s) {
