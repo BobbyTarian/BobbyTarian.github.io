57985a77f8eee931afd4676cf598bd2d8f42eb4e
BoyanP <BoyanP@users.noreply.github.com>
2025-03-25 18:23:43 -0600

    updating bandcamp link and adding cutom pixel events

--- src/components/ButtonLink.jsx
+++ src/components/ButtonLink.jsx
@@ -2,11 +2,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { propTypes } from 'react-bootstrap/esm/Image';
 
 const ButtonLink = (props) => {
-    
+    const pixelEventTrigger = (buttonText) => {
+        if (typeof window !== "undefined") {
+            if (window.fbq != null) { 
+                window.fbq('trackCustom', 'LinkClick', {buttonText: buttonText});
+            }
+        }
+        
+    }
     return (
         <div className="buttonLink">
             
-            <a className="buttonAnchorTag" href={props.url} style={props.style} target="_blank" rel="noreferrer">
+            <a className="buttonAnchorTag" href={props.url} style={props.style} target="_blank" rel="noreferrer" onClick={pixelEventTrigger()}>
                 <FontAwesomeIcon icon = {props.icon} size="1x"></FontAwesomeIcon>
                  <span className="buttonLinkText">{props.text}</span>
             </a>
