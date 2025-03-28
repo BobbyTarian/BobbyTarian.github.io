069a5c788ab903d412d3eb33bd8db02e22d8eb07
Bobby Tarian <bobbytarian@gmail.com>
2025-03-27 22:55:09 -0600

    making sure all links go to where I want them to. and open the  song

--- src/constants.jsx
+++ src/constants.jsx
@@ -1,4 +1,4 @@
-export const SPOTIFY_URL = "https://open.spotify.com/artist/5LvfXALEQIOQtnfpnAUMJX?si=uQBspdOnQLO1dBVjj2IHRQ"
+export const SPOTIFY_URL = "https://open.spotify.com/artist/5LvfXALEQIOQtnfpnAUMJX?si=uQBspdOnQLO1dBVjj2IHRQ?go=1"
 export const BANDCAMP_URL = "https://bobbytarian.bandcamp.com"
 export const SOUNDCLOUD_URL = "https://soundcloud.com/bobbytarian"
 export const INSTAGRAM_URL = "https://www.instagram.com/bobbytarian/"
@@ -8,6 +8,8 @@ export const APPLE_MUSIC_URL = "https://music.apple.com/ca/album/here-for-you/17
 export const YOUTUBE_URL = "https://www.youtube.com/@bobbytarian"
 
 export const LIMINAL_HAZE_APPLE_URL = "https://music.apple.com/us/album/a-dream-in-liminal-haze-single/1802400325"
-export const LIMINAL_HAZE_SPOTIFY_URI = "spotify:album:7KswIcAK0aMuHX1LyqGuyY"
-export const LIMINAL_HAZE_YOUTUBE_URL = "https://www.youtube.com/playlist?list=OLAK5uy_lEBaqY1O31ofhhKZWhDQ5XM8mJIT91x9s"
-export const LIMINAL_HAZE_YOUTUBE_MUSIC_URL = "https://music.youtube.com/playlist?list=OLAK5uy_lEBaqY1O31ofhhKZWhDQ5XM8mJIT91x9s"
+export const LIMINAL_HAZE_SPOTIFY_URI = "https://open.spotify.com/track/0dMMMBO0Bks1dKZJtkDoRt?si=769c5613bf86439a?context=spotify:album:7KswIcAK0aMuHX1LyqGuyY?si=cDKeTwkTS-W3BE_xfpY6DQ&go=1"
+export const LIMINAL_HAZE_YOUTUBE_URL = "https://youtu.be/4_ACtBeWuWA?si=ypzAFf9Cn3XaGUt8"
+export const LIMINAL_HAZE_YOUTUBE_MUSIC_URL = "https://music.youtube.com/watch?v=4_ACtBeWuWA&si=dykG0n8OcUinFXp1"
+
+
