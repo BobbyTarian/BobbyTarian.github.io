ed58b764bb286e76f1915df8243416cebd9853ba
Bobby Tarian <bobbytarian@gmail.com>
2025-04-06 11:59:34 -0600

    adding alt yeg magazine review to my EPK

--- src/components/MusicSection.jsx
+++ src/components/MusicSection.jsx
@@ -8,6 +8,10 @@ const musicSection = () => {
             <StreamingPlatformsDisplay></StreamingPlatformsDisplay>
             
             <br></br>
+
+            <p>A Dream in Liminal Haze <em> March 28th, 2025</em></p>
+            <iframe style={{border: 0, width: "80%", height: "120px;", margin:"auto", display:"block", padding:"1em"}} src="https://bandcamp.com/EmbeddedPlayer/track=4072734428/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/" seamless><a href="https://bobbytarian.bandcamp.com/track/a-dream-in-liminal-haze">A Dream in Liminal Haze by Bobby Tarian</a></iframe>
+
              <p>Everywhere <em>December 12th, 2024</em></p>
             <iframe style={{border: 0, width: "80%", height: "120px;", margin:"auto", display:"block", padding:"1em"}}  src="https://bandcamp.com/EmbeddedPlayer/track=1123946542/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/" seamless><a href="https://bobbytarian.bandcamp.com/track/everywhere">Everywhere by Bobby Tarian</a></iframe>
             <p>Here For You <em>March 9th, 2024</em></p>
