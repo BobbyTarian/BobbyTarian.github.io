c3b40ef1698651ffe19fb5b4fe416846f4e3c720
Bobby Tarian <bobbytarian@gmail.com>
2025-12-15 23:59:25 -0700

    removing the video cuz it was too big, hosting it on youtube instead

--- src/components/LivePerformanceSection.jsx
+++ src/components/LivePerformanceSection.jsx
@@ -1,14 +1,14 @@
 import LiminalHaze_at_Div_Bar from "../assets/Liminal_Haze_at_Dive_bar_480p.mp4"
-import theFeeling from "../assets/The_feeling.mp4"
+// import theFeeling from "../assets/The_feeling.mp4"
 
 const LivePerformanceSection = () => {
 
     return(
         <section className="LivePerformanceSection">
-            <h2> <em>The Feeling</em> live at The Aviary Dec. 11, 2025.</h2>
+            {/* <h2> <em>The Feeling</em> live at The Aviary Dec. 11, 2025.</h2>
             <video  controls="controls" >
                 <source src={theFeeling}></source>
-            </video>
+            </video> */}
             <br></br>
             <h2> <em>A Dream in Liminal Haze</em> live at Dive Bar. Oct 24, 2024.</h2>
             <video  controls="controls" >
